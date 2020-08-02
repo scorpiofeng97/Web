9e4bd3d743a644bc0e4237c34ddf1078ee69c00f
scorpiofeng97 <1805398071@qq.com>
2020-08-02 17:10:02 +0800

    update
    侧边栏配置

--- django_blog/django_blog/blog/views.py
+++ django_blog/django_blog/blog/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from .models import Post, Tag, Category
+from config.models import SideBar
 
 
 def post_list(request, category_id=None, tag_id=None):
@@ -20,7 +21,8 @@ def post_list(request, category_id=None, tag_id=None):
     context = {
         'category': category,
         'tag': tag,
-        'post_list': post_list
+        'post_list': post_list,
+        'sidebars': SideBar.get_all()
     }
     context.update(Category.get_navs())
     return render(request, 'blog/list.html', context=context)
@@ -33,6 +35,7 @@ def post_detail(request, post_id):
         post = None
     context = {
         'post': post,
+        'sidebars': SideBar.get_all()
     }
     context.update(Category.get_navs())
     return render(request, 'blog/detail.html', context=context)
